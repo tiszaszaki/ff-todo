d749a9b770b744f814bc8367c996efc5846c7e4e
atka66 <atkavagyok@gmail.com>
2021-05-20 21:10:06 +0200

    #1 base app created

---
+++ src/main/java/hu/feketefamily/ff-todo/FfTodoApplication.java
@@ -0,0 +1,13 @@
+package hu.feketefamily.spawnstrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/feketefamily/spawnstrike/FfTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.feketefamily.spawnstrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
