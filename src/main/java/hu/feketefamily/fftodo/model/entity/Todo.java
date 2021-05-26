56eafd04b9ad0ee66be8791fe43730ed0673b269
atka66 <atkavagyok@gmail.com>
2021-05-26 21:03:18 +0200

    #4 todo entity and dependencies

---
+++ src/main/java/hu/feketefamily/fftodo/model/entity/Todo.java
@@ -0,0 +1,26 @@
+package hu.feketefamily.fftodo.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "todo")
+public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String name;
+	private String description;
+}
