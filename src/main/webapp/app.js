8a4bdc9302f72dc0dcba14a3602ce6922b7d5174
atka66 <atkavagyok@gmail.com>
2021-06-06 20:30:14 +0200

    #6 create todo from ui

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,16 +1,41 @@
 var baseurl = window.location.origin;
 
-$.ajax({
-	url: baseurl + "/todo",
-	success: function(result){
-		for (var i = 0; i < result.length; i++) {
-			var cardHtml = "";
-			cardHtml += '<div class="card mb-2" style="width: 18rem;"><div class="card-body"><h5 class="card-title">';
-			cardHtml += result[i].name;
-			cardHtml += '</h5><p class="card-text">';
-			cardHtml += result[i].description
-			cardHtml += '</p></div></div>';
-			$('#todo-container').append(cardHtml);
+var fetchTodos = function () {
+	$.ajax({
+		url: baseurl + "/todo",
+		success: function(result){
+			$('#todo-container').empty();
+			for (var i = 0; i < result.length; i++) {
+				var cardHtml = "";
+				cardHtml += '<div class="card mb-2"><div class="card-body"><h5 class="card-title">';
+				cardHtml += result[i].name;
+				cardHtml += '</h5><p class="card-text">';
+				cardHtml += result[i].description
+				cardHtml += '</p></div></div>';
+				$('#todo-container').append(cardHtml);
+			}
 		}
-	}
-})
+	});
+}
+
+fetchTodos();
+
+function addTodo() {
+	$.ajax({
+		url: baseurl + "/todo",
+		method: 'PUT',
+		data: JSON.stringify({
+			name: $('#todo-name').val(),
+			description: $('#todo-description').val()
+		}),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(result){
+			$.growl.notice({message: 'Todo added successfully!'});
+			fetchTodos();
+		},
+		error: function() {
+			$.growl.error({message: 'Failed to add Todo!'});
+		}
+	})
+}
