2ab45654026eb933ef332692f42f1c718572d6f7
tiszaszaki <jobljr9717@gmail.com>
2021-07-02 15:17:04 +0200

    #21 Disable buttons for switching Todo phases

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,3 +1,11 @@
+var PHASE_CNT = 3
+
+function checkShiftTodoButtons(id, phase)
+{
+	$("#shift-todo-left-" + id).prop("disabled", !((phase > 0) && (phase < PHASE_CNT)));
+	$("#shift-todo-right-" + id).prop("disabled", !((phase >= 0) && (phase < PHASE_CNT-1)));
+}
+
 var tplCard = ({id, name, description, phase, dateModified}) => `
 <div class="card mb-2">
 	<div class="card-body">
@@ -8,15 +16,18 @@ var tplCard = ({id, name, description, phase, dateModified}) => `
 			<i class="bi bi-pencil"></i>
 		</button>
 		<button type="button" class="btn btn-danger btn-sm text-end" onclick="removeTodo(${id})"><i class="bi bi-trash"></i></button>
-		<button type="button" class="btn btn-primary btn-sm text-end"
-			onclick="shiftTodo(${id}, '${name}', '${description}', ${phase - 1})"><i class="bi bi-arrow-left"></i></button>
-		<button type="button" class="btn btn-primary btn-sm text-end"
-			onclick="shiftTodo(${id}, '${name}', '${description}', ${phase + 1})"><i class="bi bi-arrow-right"></i></button>
+		<div class="btn-group" role="group">
+			<button type="button" class="btn btn-primary btn-sm text-end"
+				id = "shift-todo-left-${id}"
+				onclick="shiftTodo(${id}, '${name}', '${description}', ${phase - 1})"><i class="bi bi-arrow-left"></i></button>
+			<button type="button" class="btn btn-primary btn-sm text-end"
+				id = "shift-todo-right-${id}"
+				onclick="shiftTodo(${id}, '${name}', '${description}', ${phase + 1})"><i class="bi bi-arrow-right"></i></button>
+		</div>
 	</div>
 	<div class="card-footer text-muted">Updated ${moment(dateModified).fromNow()}</p>
 </div>
 `
-var PHASE_CNT = 3
 
 var currentlyEditedTodoId;
 
@@ -56,9 +67,14 @@ var fetchTodos = function () {
 				$('#todo-container-phase' + i).empty();
 			}
 			for (var i = 0; i < result.length; i++) {
+				var temp_id=result[i].id;
+				var temp_phase=result[i].phase;
+
 				$('#todo-container-phase' + result[i].phase).append(
-					[{id: result[i].id, name: result[i].name, description: result[i].description, phase: result[i].phase, dateModified: result[i].dateModified}].map(tplCard)
+					[{id: temp_id, name: result[i].name, description: result[i].description, phase: temp_phase, dateModified: result[i].dateModified}].map(tplCard)
 				);
+
+				checkShiftTodoButtons(temp_id, temp_phase);
 			}
 		}
 	});
