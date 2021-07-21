36b909f2683d88c2f4099211a4cc1ca1f5485d93
tiszaszaki <jobljr9717@gmail.com>
2021-07-21 14:14:26 +0200

    #26 Create Task entity Add task on UI with modal FIX: app.js code with indentation FIX: rename some prepare functions (inserting Todo)

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -4,6 +4,7 @@ let App = {
 	baseurl: window.location.origin,
 	currentlyPickedTodoId: undefined,
 	currentlyPickedTodoName: undefined,
+	addTaskModalTitlePrefix: undefined,
 	editTodoModalTitlePrefix: undefined,
 
 	tplTask: function({task}) {
@@ -22,27 +23,64 @@ let App = {
 				<ul id="task-list-container-${id}" class="list-group p-2">
 				</ul>
 				<div class="d-flex justify-content-between">
-				<div class="btn-group" role="group">
-				<button type="button" class="btn btn-primary btn-sm text-end" onclick="App.prepareEditModal(${id}, '${name}', '${description}', '${phase}')"
-							data-bs-toggle="modal" data-bs-target="#editTodoModal" data-toggle="tooltip" data-placement="bottom" title="Edit Todo">
-					<i class="bi bi-pencil"></i>
-				</button>
-				<button type="button" class="btn btn-danger btn-sm text-end" onclick="App.prepareRemoveConfirmModal(${id}, '${name}')"
-							data-bs-toggle="modal" data-bs-target="#removeConfirmModal" data-toggle="tooltip" data-placement="bottom" title="Remove Todo">
-					<i class="bi bi-trash"></i>
-				</button>
-				</div>
-				<div class="btn-group" role="group">
-					<button type="button" class="btn btn-primary btn-sm text-end" id = "shift-todo-left-${id}"
-						onclick="App.shiftTodo(${id}, '${name}', '${description}', ${phase - 1})"
-						data-toggle="tooltip" data-placement="bottom" title="Shift Todo"><i class="bi bi-arrow-left"></i></button>
-					<button type="button" class="btn btn-primary btn-sm text-end" id = "shift-todo-right-${id}"
-						onclick="App.shiftTodo(${id}, '${name}', '${description}', ${phase + 1})"
-						data-toggle="tooltip" data-placement="bottom" title="Shift Todo"><i class="bi bi-arrow-right"></i></button>
-				</div>
+					<div class="btn-group" role="group">
+						<button type="button" class="btn btn-primary btn-sm text-end" onclick="App.prepareAddTaskModal(${id}, '${name}')"
+									data-bs-toggle="modal" data-bs-target="#addTaskModal" data-toggle="tooltip" data-placement="bottom" title="Edit Todo">
+							<i class="bi bi-plus-lg"></i>
+						</button>
+						<button type="button" class="btn btn-primary btn-sm text-end" onclick="App.prepareEditTodoModal(${id}, '${name}', '${description}', '${phase}')"
+									data-bs-toggle="modal" data-bs-target="#editTodoModal" data-toggle="tooltip" data-placement="bottom" title="Edit Todo">
+							<i class="bi bi-pencil"></i>
+						</button>
+						<button type="button" class="btn btn-danger btn-sm text-end" onclick="App.prepareRemoveConfirmModal(${id}, '${name}')"
+									data-bs-toggle="modal" data-bs-target="#removeConfirmModal" data-toggle="tooltip" data-placement="bottom" title="Remove Todo">
+							<i class="bi bi-trash"></i>
+						</button>
+					</div>
+					<div class="btn-group" role="group">
+						<button type="button" class="btn btn-primary btn-sm text-end" id = "shift-todo-left-${id}"
+							onclick="App.shiftTodo(${id}, '${name}', '${description}', ${phase - 1})"
+							data-toggle="tooltip" data-placement="bottom" title="Shift Todo"><i class="bi bi-arrow-left"></i></button>
+						<button type="button" class="btn btn-primary btn-sm text-end" id = "shift-todo-right-${id}"
+							onclick="App.shiftTodo(${id}, '${name}', '${description}', ${phase + 1})"
+							data-toggle="tooltip" data-placement="bottom" title="Shift Todo"><i class="bi bi-arrow-right"></i></button>
+					</div>
 				</div>
 			</div>
 			<div class="card-footer text-muted">Updated ${moment(dateModified).fromNow()}</p>
+		</div>
+		`
+	},
+	tplTaskModal: function({modalPrefix, modalTitle, placeholderName, submitButtonCaption, dismissButtonCaption})
+	{
+		return `
+		<div class="modal fade" id="${modalPrefix}TaskModal" tabindex="-1">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="${modalPrefix}-task-title">${modalTitle}</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+					</div>
+					<div class="modal-body">
+						<form novalidate>
+							<div id="${modalPrefix}-task-name-form-group" class="mb-3">
+								<label for="${modalPrefix}-task-name" class="col-form-label">Name:</label>
+								<input type="text" class="form-control" id="${modalPrefix}-task-name"
+									   placeholder="${placeholderName}"
+									   oninput="App.validateFormGroup('${modalPrefix}-task-name-form-group')" required>
+								<div id="${modalPrefix}-task-name-validator" class="invalid-feedback">
+									Please provide a name.
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" id="${modalPrefix}-task-submit-button"
+							onclick="App.${modalPrefix}Task()" data-bs-dismiss="modal">${submitButtonCaption}</button>
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">${dismissButtonCaption}</button>
+					</div>
+				</div>
+			</div>
 		</div>
 		`
 	},
@@ -75,7 +113,7 @@ let App = {
 								<span class="pull-right label label-default" id="${modalPrefix}-todo-description-char-count"></span>
 							</div>
 							<div>
-							<label class="col-form-label">Phase:</label>
+								<label class="col-form-label">Phase:</label>
 							</div>
 							<div class="btn-group" role="group">
 								<input type="radio" class="btn-check" name="${modalPrefix}-todo-phase" id="${modalPrefix}-todo-phase0" value="0" checked>
@@ -117,7 +155,8 @@ let App = {
 						<button type="button" class="btn btn-danger" id="${modalPrefix}-confirm-modal-submit-button">${submitButtonCaption}</button>
 						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">${dismissButtonCaption}</button>
 					</div>
-				</div>			</div>
+				</div>
+			</div>
 		</div>
 		`
 	},
@@ -131,10 +170,10 @@ let App = {
 
 		$('#' + modalPrefix + '-todo-description-char-count').html(charCountStr);
 	},
-	prepareAddModal: function() {
+	prepareAddTodoModal: function() {
 		App.setTodoModalCharCount('add');
 	},
-	prepareEditModal: function(id, name, description, phase) {
+	prepareEditTodoModal: function(id, name, description, phase) {
 		App.currentlyPickedTodoId = id;
 
 		$('#edit-todo-title').html(editTodoModalTitlePrefix + name);
@@ -144,6 +183,12 @@ let App = {
 
 		App.setTodoModalCharCount('edit');
 	},
+	prepareAddTaskModal: function(id, name) {
+		App.currentlyPickedTodoId = id;
+		App.currentlyPickedTodoName = name;
+
+		$('#add-task-title').html(addTaskModalTitlePrefix + name);
+	},
 	fetchTodos: function () {
 		$.ajax({
 			url: App.baseurl + "/todo",
@@ -152,8 +197,8 @@ let App = {
 					$('#todo-container-phase' + i).empty();
 				}
 				for (var i = 0; i < result.length; i++) {
-					var temp_id=result[i].id;
-					var temp_phase=result[i].phase;
+					var temp_id = result[i].id;
+					var temp_phase = result[i].phase;
 					var temp_tasks = result[i].tasks;
 
 					$('#todo-container-phase' + result[i].phase).append(
@@ -253,16 +298,42 @@ let App = {
 			$.growl.warning({message: 'Todo (' + name2 + ') cannot be shifted this way!'});
 		}
 	},
+	addTask: function() {
+		var todo_id = App.currentlyPickedTodoId;
+		var todo_name = App.currentlyPickedTodoName;
+		var task_name = $('#add-task-name').val();
+
+		$.ajax({
+			url: App.baseurl + "/todo/" + todo_id + "/task",
+			method: 'PUT',
+			data: JSON.stringify({
+				name: task_name,
+				done: false,
+			}),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(){
+				$.growl.notice({message: 'Task (' + task_name + ') added successfully for Todo (' + todo_name + ')!'});
+				$('#add-todo-name').val('');
+				$('#add-todo-description').val('');
+				$('#add-todo-phase0').prop("checked", true);
+				App.fetchTodos();
+			},
+			error: function() {
+				$.growl.error({message: 'Failed to add Task (' + task_name + ')!'});
+			}
+		});
+	},
 	validateFormGroup: function(formGroupId) {
 		var fromGroup = $('#' + formGroupId);
 		if(!fromGroup.hasClass("was-validated")){
 			fromGroup.addClass("was-validated");
 		}
 	},
-	submitTodoModal: function(e, modalPrefix)
+	submitModal: function(e, modalPrefix)
 	{
 		if (e.which == 13) {
-			$('#' + modalPrefix + '-todo-submit-button').click();
+			$('#' + modalPrefix + '-submit-button').click();
 			e.preventDefault();
 		}
 	},
@@ -273,7 +344,7 @@ let App = {
 				submitButtonCaption: 'Add', dismissButtonCaption: 'Close'}].map(App.tplTodoModal)
 		);
 
-		$('#add-todo-name').keypress(function(e) { App.submitTodoModal(e, 'add'); });
+		$('#add-todo-name').keypress(function(e) { App.submitModal(e, 'add-todo'); });
 
 		editTodoModalTitlePrefix = 'Edit todo: ',
 
@@ -284,6 +355,16 @@ let App = {
 		);
 
 		$('#edit-todo-name').keypress(function(e) { App.submitTodoModal(e, 'edit'); });
+
+		addTaskModalTitlePrefix = 'Add task for this Todo: ',
+
+		$('#modal-container').append(
+			[{modalPrefix: 'add', modalTitle: '',
+				placeholderName: 'Enter name for new Task...',
+				submitButtonCaption: 'Add', dismissButtonCaption: 'Close'}].map(App.tplTaskModal)
+		);
+
+		$('#add-task-name').keypress(function(e) { App.submitModal(e, 'add-task'); });
 
 		$('#modal-container').append(
 			[{modalPrefix: 'remove', modalTitle: 'Confirm removing a Todo', modalMessage: '',
@@ -307,6 +388,7 @@ let App = {
 	{
 		App.currentlyPickedTodoId = id;
 		App.currentlyPickedTodoName = name;
+
 		$('#remove-confirm-modal-message').html('Are you sure to remove this Todo (' + name + ')?');
 	}
 }
