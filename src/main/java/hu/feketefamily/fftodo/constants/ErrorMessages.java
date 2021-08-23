8c4b34f5aeb5092cb42dd68b70c096e81fc2cce2
tiszaszaki <jobljr9717@gmail.com>
2021-08-23 13:56:06 +0200

    #47 Create TodoCommon within backend FIX: ErrorMessages fields should be final

--- src/main/java/hu/feketefamily/fftodo/constants/ErrorMessages.java
+++ src/main/java/hu/feketefamily/fftodo/constants/ErrorMessages.java
@@ -1,7 +1,7 @@
 package hu.feketefamily.fftodo.constants;
 
 public class ErrorMessages {
-	public static String CONSTRAINT_VIOLATION_MESSAGE = "Constraint violation occurred";
-	public static String TODO_NOT_EXIST_MESSAGE = "Todo does not exist!";
-	public static String TASK_NOT_EXIST_MESSAGE = "Task does not exist!";
+	public static final String CONSTRAINT_VIOLATION_MESSAGE = "Constraint violation occurred";
+	public static final String TODO_NOT_EXIST_MESSAGE = "Todo does not exist!";
+	public static final String TASK_NOT_EXIST_MESSAGE = "Task does not exist!";
 }
---
+++ src/main/java/hu/feketefamily/fftodo/constants/TodoCommon.java
@@ -0,0 +1,7 @@
+package hu.feketefamily.fftodo.constants;
+
+public class TodoCommon {
+	public static final Integer maxDescriptionLength = 1024;
+	public static final Integer phaseMin = 0;
+	public static final Integer phaseMax = 2;
+}
